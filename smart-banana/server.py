d650a472ad179c2f9416ffb60fb1ad2857896484
r-iradukunda <r.iradukund@alustudent.com>
2025-11-02 22:37:06 +0200

    fix deploy

--- smart-banana/server.py
+++ smart-banana/server.py
@@ -201,5 +201,5 @@ def test_rejection():
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
 
