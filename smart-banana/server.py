4a842642baf7bee96cf48fb603a7dcf846fba88d
r-iradukunda <r.iradukund@alustudent.com>
2025-11-06 02:17:47 +0200

    Remove .keras and .h5 from LFS tracking tesy

--- smart-banana/server.py
+++ smart-banana/server.py
@@ -199,4 +199,6 @@ def test_rejection():
     })
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000)) 
+    app.run(host='0.0.0.0', port=port)
